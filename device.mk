056561105946ea39663cacc0f7b21b3e46fb62a2
xyyx <xyyx@mail.ru>
2016-01-08 15:15:59 +0800

    mako: update media codec configuration Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- device.mk
+++ device.mk
@@ -69,6 +69,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ueventd.geeb.rc:root/ueventd.geeb.rc \
     $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml 
