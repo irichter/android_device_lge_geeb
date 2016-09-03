67ec82e726575ba19e1b49b93f18db30c6cf70ce
Isaac Richter <laughingjudge@gmail.com>
2016-09-02 23:59:23 -0400

    nonvert nitrogen_geeb.mk to du.mk

--- du.mk
+++ du.mk
@@ -2,24 +2,19 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
-# Boot animation
-#PRODUCT_COPY_FILES += \
-#    vendor/nitrogen/prebuilt/common/bootanimations/bootanimation_geeb.zip:system/media/bootanimation.zip
-
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/nitrogen/products/common.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/gsm.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/geeb/full_geeb.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := geeb
-PRODUCT_NAME := nitrogen_geeb
+PRODUCT_NAME := du_geeb
 PRODUCT_BRAND := LGE
-PRODUCT_MODEL := Optimus G
-PRODUCT_MANUFACTURER := LGE
-PRODUCT_RESTRICT_VENDOR_FILES := false
+
+$(call inherit-product-if-exists, vendor/lge/mako/mako-vendor.mk)
 
